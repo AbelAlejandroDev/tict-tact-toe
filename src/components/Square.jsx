f2f5d23ef3159af3d0baa32875086080c9b30a97
AbelAlejandroDev <pyschofaint@gmail.com>
2024-04-27 14:21:55 -0400

    Redesign Square component

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,6 +1,8 @@
 export const Square = ({onSquareClick,value}) => {
   return (
-    <button className="square" onClick={onSquareClick}>
+    <button 
+    className="w-16 h-16  bg-white border border-gray-300 text-3xl font-bold flex justify-center items-center"
+      onClick={onSquareClick}>
       {value}
     </button>
   )
