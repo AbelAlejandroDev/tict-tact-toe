b5cfdbe463f74d8046d8d37afef5bc78ee7f46a5
AbelAlejandroDev <pyschofaint@gmail.com>
2024-04-28 10:14:37 -0400

    Add to context token, isX

--- src/context/SocketContext.jsx
+++ src/context/SocketContext.jsx
@@ -4,10 +4,12 @@ import { useSocket } from "../hooks/useSocket";
 export const SocketContext = createContext();
 
 export const SocketProvider = ({ children }) => {
-  const { socket, online,gameMode,isX } = useSocket("http://localhost:8080");
+  const { socket, online, gameMode, isX, token } = useSocket(
+    "http://localhost:8080"
+  );
 
   return (
-    <SocketContext.Provider value={{ socket, online,gameMode,isX }}>
+    <SocketContext.Provider value={{ socket, online, gameMode, isX, token }}>
       {children}
     </SocketContext.Provider>
   );
