91d0ca127745cee499d37377b11147276a2428be
AbelAlejandroDev <pyschofaint@gmail.com>
2024-04-28 07:48:50 -0400

    Add plays functions and notification answer

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,61 +1,72 @@
 import { useCalculateWinner } from "../hooks/useCalculateWinner";
 import { Square } from "./Square";
-import Swal from "sweetalert2";
 import { Status } from "./Status";
-import { useGame } from "../hooks/useGame";
+import { useContext, useEffect, useState } from "react";
+import { SocketContext } from "../context/SocketContext";
 
 export const Board = ({ xIsNext, squares, onPlay }) => {
+  const { socket, token } = useContext(SocketContext);
   const winner = useCalculateWinner(squares);
-  const {jumpTo}= useGame()
+  const [currentPlayer, setCurrentPlayer] = useState(null);
+  const [answer, setAnswer] = useState(null);
+
+  socket.on("jugador-unido", ({ players }) => {
+    // Si hay dos jugadores, establecer al primer jugador como el jugador actual
+    setCurrentPlayer(players[0]);
+  });
+
+  useEffect(() => {
+    socket.on("jugada", ({ squares, currentPlayer }) => {
+      // Actualizamos el estado del juego con el movimiento recibido del servidor
+      onPlay(squares);
+      setCurrentPlayer(currentPlayer);
+    });
+
+    // Limpiamos  el efecto
+    return () => {
+      socket.off("jugada");
+    };
+  }, [socket, onPlay]);
+
+  useEffect(() => {
+    socket.on("respuesta-revancha", (accepted) => {
+      setAnswer(accepted);
+    });
+  }, [socket]);
+
   function handleClick(i) {
-    if (winner || squares[i]) {
-      return;
+    if (currentPlayer !== socket.id || winner || squares[i]) {
+      return console.log(currentPlayer);
     }
+
+    console.log(currentPlayer);
     const nextSquares = squares.slice();
     if (xIsNext) {
       nextSquares[i] = "X";
     } else {
       nextSquares[i] = "O";
     }
-    onPlay(nextSquares);
-  }
-
-  if (winner) {
-    Swal.fire({
-      title: "Winner " + winner,
-      icon: "info",
-      confirmButtonText: "Thanks to play",
-      iconColor: "green",
+    socket.emit("jugada", {
+      code: token ?? sessionStorage.getItem(token),
+      squares: nextSquares,
+      currentPlayer,
     });
   }
 
-  let found=squares.every((element) => element !== null)
-
-  if (!winner && found ) {
-    Swal.fire({
-      title: "Empate",
-      icon: "info",
-      confirmButtonText: "Thanks to play",
-    });
-  }
+  let found = squares.every((element) => element !== null);
 
   return (
     <>
-      <Status xIsNext={xIsNext} found={found}  />
-      <div className="board-row">
-        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+      {answer === false && <h1>NO quiero pesao</h1>}
+      <Status xIsNext={xIsNext} token={token} found={found} winner={winner} />
+      <div className="grid grid-cols-3 gap-0 w-48 h-48 mb-4">
+        {squares.map((value, index) => (
+          <Square
+            key={index}
+            value={value}
+            onSquareClick={() => handleClick(index)}
+          />
+        ))}
       </div>
     </>
   );
