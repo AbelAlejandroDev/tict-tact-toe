26a3c5d8ce2ac7cafd7c542b1f99d12601718830
AbelAlejandroDev <pyschofaint@gmail.com>
2024-04-28 22:30:11 -0400

    Update, Add prop reject to notifications

--- src/components/NotificationBubble.jsx
+++ src/components/NotificationBubble.jsx
@@ -1,12 +1,12 @@
 import { useState, useEffect } from "react";
 
-const NotificationBubble = ({ message, success, error }) => {
+const NotificationBubble = ({ message, success, error, rejet }) => {
   const [visible, setVisible] = useState(true);
 
   useEffect(() => {
     const timer = setTimeout(() => {
       setVisible(false);
-    }, 2000); 
+    }, 2000);
 
     return () => clearTimeout(timer);
   }, []);
@@ -18,12 +18,33 @@ const NotificationBubble = ({ message, success, error }) => {
           ? "opacity-100 duration-300"
           : "opacity-0 duration-300 pointer-events-none"
       }`}
-      style={{ backgroundColor: success ? "#2F855A" : error ? "#E53E3E" : "#4A5568" }}
+      style={{
+        backgroundColor: success
+          ? "#2F855A"
+          : error || rejet
+          ? "#E53E3E"
+          : "#4A5568",
+      }}
     >
       {success && (
         <>
           <div className="flex flex-row items-center justify-center">
             <img className="h-6 w-6" src="check.svg" alt="Success join" />
+            <small className="mx-2  text-sm">{message}</small>
+          </div>
+        </>
+      )}
+      {error && (
+        <>
+          <div className="flex flex-row items-center justify-center">
+            <img className="h-6 w-6" src="check.svg" alt="Success join" />
+            <small className="mx-2  text-sm">{message}</small>
+          </div>
+        </>
+      )}
+      {rejet && (
+        <>
+          <div className="flex flex-row items-center justify-center">
             <small className="mx-2  text-sm">{message}</small>
           </div>
         </>
